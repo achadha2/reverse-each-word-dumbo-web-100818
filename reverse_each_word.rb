fee103be8dedbf601f4f1e9df176b69b54f75850
andy.chadha2@gmail.com <andy.chadha2@gmail.com>
2018-09-11 19:31:08 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@
 def reverses_each_word(words)
   new_array = []
   array = words.split(' ')
-  array.each {|word| word.reverse << new_array}
+  array.each {|word| "#{word.reverse}" << new_array}
   new_array.join(" ")
   
 end
